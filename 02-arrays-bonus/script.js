e30fca503d369ae31007e41869ead0cfba46eab4
GiorgiaSecchi <secchigrg@gmail.com>
2024-10-16 17:19:54 +0200

    3 - bonus

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -8,7 +8,11 @@ console.log(reversedTeachers);
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
+
 const longNames = teachers.filter((teachers) => teachers.length >= 5);
 console.log(longNames);
 
 // 3. Rimuovi 'Ed' dall'array teachers
+
+teachers.splice(5, 1);
+console.log(teachers);
