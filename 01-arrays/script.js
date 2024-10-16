3016ce8c919ee142efc546669d1ad92c62f4ffbd
GiorgiaSecchi <secchigrg@gmail.com>
2024-10-16 15:48:40 +0200

    5 - es.

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -2,6 +2,7 @@ const teachers = ["Nathan", "Ed", "Fabio", "Phil", "Carlo", "Lewis", "Luca"]; //
 
 // 1. Utilizzando un indice sull'array teachers, cambia il valore della
 // variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
+
 const fourthTeacher = teachers[3];
 console.log(fourthTeacher);
 
@@ -11,14 +12,19 @@ teachers[4] = "Patrick";
 console.log(teachers);
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
+
 const lastTeacher = teachers.pop(6);
 console.log(teachers);
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
+
 const firstTeacher = teachers.shift(0);
 console.log(teachers);
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
+
+teachers.push("Vanessa");
+console.log(teachers);
 
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
 
